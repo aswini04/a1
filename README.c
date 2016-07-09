faf4eee79ed0231aae7eeaa5c2207c4ca6423d1b
aswini04 <aswini04081995@gmail.com>
2016-07-09 15:52:34 +0530

    finish

---
+++ README.c
@@ -0,0 +1,52 @@
+# a1
+#include<stdio.h>
+#include<conio.h>
+main() //------main function-----//
+{
+
+int x,i=0;
+printf("Press 1 By For Loop:\nPress 2 By While Loop:\nPress 3 By Do while loop:\n");
+scanf("%d",&x);
+switch(x)
+{
+case 1:
+printf("\nPrinting of Hello World by using For Loop\n");
+
+for(i=0;i<10;i++)
+{
+printf("Hello World\n");
+}
+break;
+case 2:
+printf("\nPrinting of Hello World by using While Loop\n");
+
+while(i<10)
+{
+printf("Hello World\n");
+i++;
+}
+break;
+case 3:
+printf("\nPrinting of Hello World by using Do While Loop\n");
+
+do
+{
+printf("Hello World\n");
+i++;
+}
+case 4:
+printf("\nPrinting of Hello World by using Do While Loop\n");
+
+do
+{
+printf("Hello World\n");
+i++;
+}
+case 5:
+printf("\nPrinting of Hello World by using Do While Loop\n");
+
+do
+{
+printf("Hello World\n");
+i++;
+}
